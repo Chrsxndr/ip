2d5cc57c8db07219fad0fd4f6318fec1144d4cbe
Christian Alexander <249088917+Chrsxndr@users.noreply.github.com>
2026-08-21 07:07:34 +0800

    Fix bug in echo

--- src/main/java/Clarry.java
+++ src/main/java/Clarry.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Clarry {
     public static void main(String[] args) {
         String banner = "  _____ _\n"
@@ -13,7 +15,23 @@ public class Clarry {
         System.out.println(" Hello! I'm Clarry.");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + input);
+            System.out.println("____________________________________________________________");
+        }
+
+        scanner.close();
     }
 }
