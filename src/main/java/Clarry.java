e08c83f6d4b2cdf0935f492d59bb8d7cb730001b
Christian Alexander <249088917+Chrsxndr@users.noreply.github.com>
2026-08-21 07:15:32 +0800

    Add support for list

--- src/main/java/Clarry.java
+++ src/main/java/Clarry.java
@@ -15,7 +15,10 @@ public class Clarry {
         System.out.println(" Hello! I'm Clarry.");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
+
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
 
         while (true) {
             String input = scanner.nextLine();
@@ -25,11 +28,19 @@ public class Clarry {
                 System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+            } else if (input.equals("list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println("____________________________________________________________");
+                System.out.println(" added: " + input);
+                System.out.println("____________________________________________________________");
             }
-
-            System.out.println("____________________________________________________________");
-            System.out.println(" " + input);
-            System.out.println("____________________________________________________________");
         }
 
         scanner.close();
