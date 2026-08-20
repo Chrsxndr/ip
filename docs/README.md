0c3b0e0c13cc1f0592d61022654ff4aff0d62ddd
Christian Alexander <249088917+Chrsxndr@users.noreply.github.com>
2026-08-21 06:59:13 +0800

    Add greeting and exit

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Clarry User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Clarry.java
@@ -0,0 +1,19 @@
+public class Clarry {
+    public static void main(String[] args) {
+        String banner = "  _____ _\n"
+                        + " / ____| |\n"
+                        + "| |    | | __ _ _ __ _ __ _   _\n"
+                        + "| |    | |/ _` | '__| '__| | | |\n"
+                        + "| |____| | (_| | |  | |  | |_| |\n"
+                        + " \\_____|_|\\__,_|_|  |_|  \\__,  |\n"
+                        + "                          __/  |\n"
+                        + "                         |____/ \n";
+        System.out.println(banner);
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm Clarry.");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
